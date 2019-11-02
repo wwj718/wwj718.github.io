f3a4e7b94e29b5dd0ff395107a4d74436a604732
wwj718 <wuwenjie718@gmail.com>
2019-11-02 10:58:44 +0800

    rebuilding site 2019年11月 2日 星期六 10时58分44秒 CST

--- download_just4fun_img.py
+++ download_just4fun_img.py
@@ -5,11 +5,14 @@ ack -ho "http:[^\(\)\[\]]*?\.(png|jpg|jpeg|gif)" > /tmp/just4fun_img_list.txt
 
 
 ack -ho "http:[^\(\)\[\]]*?\.(PNG|JPG|JPEG)" > /tmp/big_just4fun_img_list.txt
+
+# 所有mp4
+ack -ho "http://.*?.(mp4|MP4)" > /tmp/blog_mp4_list.txt
 '''
 
 import subprocess
 # with open("/tmp/just4fun_img_list.txt") as png_url_list:
-with open("/tmp/big_just4fun_img_list.txt") as png_url_list:
+with open("/tmp//tmp/blog_mp4_list.txt") as png_url_list:
     urls = set(png_url_list.readlines()) # 去重
     # print(len(urls))
 for url in urls: 
@@ -18,4 +21,4 @@ for url in urls:
         # print(url)
         subprocess.call(cmd,shell=True) # error timeout
     else:
-        print(urls)
+        print(url)
