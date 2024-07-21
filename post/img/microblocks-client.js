21e0aa428fe395a7c35bebe9d369fb2ee70878c4
wwj718 <wuwenjie718@gmail.com>
2024-07-21 21:26:14 +0800

    rebuilding site Sun Jul 21 21:26:14 CST 2024

--- post/img/microblocks-client.js
+++ post/img/microblocks-client.js
@@ -16,22 +16,18 @@ class MicroBlocksClient {
     }
 
     send(aString) {
-        console.log(aString);
+        // console.log(aString);
         const data = new TextEncoder().encode(aString);
         let length = data.length + 1;
         let bytes = new Uint8Array([...(new Uint8Array([251, 27, 0, length % 256, parseInt((length / 256))])), ...data, ...(new Uint8Array([254]))])
-
-        const MICROBLOCKS_SERVICE_UUID = "bb37a001-b922-4018-8e74-e14824b3a638"
-        const MICROBLOCKS_RX_CHAR_UUID = "bb37a002-b922-4018-8e74-e14824b3a638" // board receive characteristic
-        const MICROBLOCKS_TX_CHAR_UUID = "bb37a003-b922-4018-8e74-e14824b3a638" // board transmit characteristic
-
-        window.bleDevice && window.bleDevice.gatt
-            .getPrimaryService(this.MICROBLOCKS_SERVICE_UUID)
-            .then((service) => service.getCharacteristic(this.MICROBLOCKS_RX_CHAR_UUID))
-            .then((characteristic) => characteristic.writeValue(bytes))
-            .catch((error) => {
-                console.log("error:" + error);
-            });
+        if (window.bleDevice) {
+            window.bleDevice.gatt
+                .getPrimaryService(this.MICROBLOCKS_SERVICE_UUID)
+                .then((service) => service.getCharacteristic(this.MICROBLOCKS_RX_CHAR_UUID))
+                .then((characteristic) => characteristic.writeValue(bytes))
+                .catch((error) => {
+                    console.log("error:" + error);
+                });
+        }
     }
-
 }
